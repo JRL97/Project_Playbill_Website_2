3eb76f1c3e0c84791f60397084ecee206b3df9ba
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-05-13 02:23:53 +0100

    Fake data entered into table but not interated 5

--- seed.php
+++ seed.php
@@ -13,18 +13,20 @@
     //x5
 
     $faker = Faker\Factory::create();
-     
-    for ($i=0; $i < 5; $i++;) {
+
+   
+    for ($i=0; $i < 5; $i++) {
+
+      $student_id =  $faker->unique()->numberBetween(20000001, 29999999);
 
      $sql = "INSERT INTO student (studentid, password, dob, firstname, 
       lastname, house, town, county, country, postcode)
       VALUES ('$student_id', '{$faker->password}', 
       '{$faker->date}', '{$faker->firstName}', '{$faker->lastName}', '{$faker->word}', 
-      '{$faker->city}', '{$faker->city}', '{$faker->country}', '{$faker->postcode}')
-  ");
+      '{$faker->city}', '{$faker->city}', '{$faker->country}', '{$faker->postcode}')";
 
+   
+    } 
     $result = mysqli_query($conn,$sql);
-    }
-
-   } 
+    echo "5 records successfully inserted.";
 ?>
