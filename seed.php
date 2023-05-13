9ff51abcccf80f5fc6b3a94a4ad16f0385be96a1
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-05-13 01:32:12 +0100

    Composer faker added and broken seed page

--- seed.php
+++ seed.php
@@ -4,15 +4,24 @@
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
 
+   require_once 'vendor/autoload.php';
 
    //check if we are logged in
-   if (isset($_SESSION['id'])) {
+   //if (isset($_SESSION['id'])) {
     // build inster query 
     // run query 
     //x5
+
+    $faker = Faker\Factory::create();
      
     for ($i=0; $i < 5; $i++;) {
-    $sql = "INSERT INTO student";
+
+     $sql = "INSERT INTO student (studentid, password, dob, firstname, 
+      lastname, house, town, county, country, postcode)
+      VALUES ('$student_id', '{$faker->password}', 
+      '{$faker->date}', '{$faker->firstName}', '{$faker->lastName}', '{$faker->word}', 
+      '{$faker->city}', '{$faker->city}', '{$faker->country}', '{$faker->postcode}')
+  ");
 
     $result = mysqli_query($conn,$sql);
     }
