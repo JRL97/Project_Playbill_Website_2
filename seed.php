9db3f060d588b5673486356f156626c29c10e1ad
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-05-14 01:29:06 +0100

    Task 1 Complete

--- seed.php
+++ seed.php
@@ -6,27 +6,21 @@
 
    require_once 'vendor/autoload.php';
 
-   //check if we are logged in
-   //if (isset($_SESSION['id'])) {
-    // build inster query 
-    // run query 
-    //x5
-
-    $faker = Faker\Factory::create();
+   $faker = Faker\Factory::create();
 
    
-    for ($i=0; $i < 5; $i++) {
+   for ($i=0; $i < 5; $i++) {
 
       $student_id =  $faker->unique()->numberBetween(20000001, 29999999);
 
-     $sql = "INSERT INTO student (studentid, password, dob, firstname, 
+      $sql = "INSERT INTO student (studentid, password, dob, firstname, 
       lastname, house, town, county, country, postcode)
       VALUES ('$student_id', '{$faker->password}', 
-      '{$faker->date}', '{$faker->firstName}', '{$faker->lastName}', '{$faker->word}', 
-      '{$faker->city}', '{$faker->city}', '{$faker->country}', '{$faker->postcode}')";
-
-   
+      '{$faker->date}', '{$faker->firstName}', '{$faker->lastName}', '{$faker->address}', 
+      '{$faker->city}', '{$faker->state}', '{$faker->country}', '{$faker->postcode}')";
+      $result = mysqli_query($conn,$sql);
+      echo "Student number " . $i . " has been added to the database! ";
     } 
-    $result = mysqli_query($conn,$sql);
-    echo "5 records successfully inserted.";
+    
+  
 ?>
