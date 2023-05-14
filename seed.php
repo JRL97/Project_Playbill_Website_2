4e1d4896ffbafd63fc0375a3848093893b668101
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-05-14 02:26:03 +0100

    seed comments and task 2 complete

--- seed.php
+++ seed.php
@@ -4,11 +4,13 @@
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
 
+   //Faker Library Composer Required
    require_once 'vendor/autoload.php';
 
+   // New instance of Faker Library
    $faker = Faker\Factory::create();
 
-   
+   //Insert 5 students with unique IDs into database
    for ($i=0; $i < 5; $i++) {
 
       $student_id =  $faker->unique()->numberBetween(20000001, 29999999);
@@ -19,6 +21,8 @@
       '{$faker->date}', '{$faker->firstName}', '{$faker->lastName}', '{$faker->address}', 
       '{$faker->city}', '{$faker->state}', '{$faker->country}', '{$faker->postcode}')";
       $result = mysqli_query($conn,$sql);
+
+      //echo out each student successfullhy created
       echo "Student number " . $i . " has been added to the database! ";
     } 
     
