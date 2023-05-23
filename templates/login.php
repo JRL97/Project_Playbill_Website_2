01947ee333219f5efdaf83a210d636fb93f73c55
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-05-23 16:59:36 +0100

    Bootstrap changes

--- templates/login.php
+++ templates/login.php
@@ -1,12 +1,25 @@
+<div class="bg-image"
+     style="background-image: url('images/BucksLogo.jpg'); background-repeat:no-repeat; height: 100vh;">
+     
+<h1 class="mt-4">Login </h1>
 
-<?php echo $message; ?>
+<?php 
+if (!empty($message)) {
+   echo '<div class="alert alert-danger" role="alert">' . $message . '</div';
+}
+?>
 
 <form name="frmLogin" action="authenticate.php" method="post">
-   Student ID:
-   <input name="txtid" type="text" />
-   <br/>
-   Password:
-   <input name="txtpwd" type="password" />
-   <br/>
-   <input type="submit" value="Login" name="btnlogin" />
+  <div class="mt-3">
+    <label for="txtid" class="form-label">Student ID</label>
+    <input type="text" class="form-control" id="txtid" name="txtid">
+  </div>
+  <div class="mt-3">
+    <label for="txtpwd" class="form-label">Password</label>
+    <input type="password" class="form-control" id="txtpwd" name="txtpwd">
+  </div>
+  <div class="mt-4">
+  <input type="submit" name="btnlogin" class="btn btn-primary" value="Login"/>
+  </div>
 </form>
+</div>
